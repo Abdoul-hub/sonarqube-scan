c58b29db687a9499ae88f444e9f0e7deb9b551c5
togola080925 <abdoulkarim.togola@orangemali.com>
2026-04-04 09:55:38 +0000

    first commit

---
+++ src/test/java/com/cabral/ExampleResourceIT.java
@@ -0,0 +1,8 @@
+package com.cabral;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class ExampleResourceIT extends ExampleResourceTest {
+    // Execute the same tests but in packaged mode.
+}
